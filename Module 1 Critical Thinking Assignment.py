e109eab6c959ff83fd15be729973d29fe79e17dc
mark-stanton-csuglobal-edu <mark.stanton@csuglobal.edu>
2025-03-23 19:05:03 -0700

    Add files via upload

---
+++ Module 1 Critical Thinking Assignment.py
@@ -0,0 +1,15 @@
+#Part 1: Write a Python program to find the addition and subtraction of two numbers
+def main():
+    num1 = int(input('Enter a number: '))
+    num2 = int(input('Enter another number: '))
+    print('The sum of the first and second numbers is', num1 + num2)
+    print('The difference between the first and second numbers is', num1 - num2)
+if __name__ == '__main__': main()
+
+#Part 2: Write a Python program to find the multiplication and division of two numbers
+def main():
+    num3 = int(input('Enter a number: '))
+    num4 = int(input('Enter another number: '))
+    print('The product of the first and second numbers is', num3 * num4)
+    print('The quotient of the first and second numbers is', num3 / num4)
+if __name__ == '__main__': main()
